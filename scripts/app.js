01744a7d908adc8c441ee37c9a8bc05d32a09e5c
Mark Lyen <mark@marklyen.com>
2017-02-12 11:41:42 -0800

    Refactor header/footer into shared files.  Added 2 new photos.

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
+var App;
+(function (App) {
+    $("header").load("../header.html");
+    $("footer").load("../footer.html");
+})(App || (App = {}));
+//# sourceMappingURL=app.js.map
---
+++ scripts/app.ts
@@ -0,0 +1,5 @@
+﻿/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
+namespace App {
+    $("header").load("../header.html");
+    $("footer").load("../footer.html");
+}

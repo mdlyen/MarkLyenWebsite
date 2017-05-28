1d8be9f06f92b712a79151a457b93bdc415a0445
Mark Lyen <mark@marklyen.com>
2017-05-28 14:25:07 -0700

    Updating Typescript version.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
+/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
 var App;
 (function (App) {
     $("header").load("../header.html");
     $("footer").load("../footer.html");
 })(App || (App = {}));
+//# sourceMappingURL=app.js.map
